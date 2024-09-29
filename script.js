6ad8d3bcf50db69ff08f23e38d9f5a61b850839c
Mks <mukundsrinivasan0@gmail.com>
2024-09-29 19:59:48 +1000

    changed api

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ document.getElementById('new-quote').addEventListener('click', fetchQuote);
 
 async function fetchQuote() {
     try {
-        const response = await fetch('https://api.quotable.io/random');
+        const response = await fetch('https://quotes-api-self.vercel.app/quote');
         const data = await response.json();
-        document.getElementById('quote').innerText = data.content;
+        document.getElementById('quote').innerText = data.quote;
         document.getElementById('author').innerText = "- " + data.author;
     } catch (error) {
         console.error('Error fetching quote:', error);
