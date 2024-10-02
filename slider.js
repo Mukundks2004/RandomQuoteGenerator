e9928188daaa84d1a5dabd92ec45191eb38c4f54
Mks <mukundsrinivasan0@gmail.com>
2024-10-03 00:03:13 +1000

    added lana's face. born to die audio, now we play music, changed css a bit

--- slider.js
+++ slider.js
@@ -1,4 +1,5 @@
 let currentSlide = 0;
+const music_dur = 0.5;
 
 function changeSlide(direction) {
     const slides = document.querySelectorAll('.slide');
@@ -8,6 +9,10 @@ function changeSlide(direction) {
 
     const slideContainer = document.querySelector('.slides');
     slideContainer.style.transform = `translateX(-${currentSlide * 100}%)`;
+
+    const currentSlideElement = slides[currentSlide];
+    const note = currentSlideElement.getAttribute('data-note');
+    playNote(noteFrequencies[note][0], noteFrequencies[note][1]);
 }
 
 document.addEventListener('keydown', function(event) {
@@ -15,5 +20,51 @@ document.addEventListener('keydown', function(event) {
         changeSlide(1);
     } else if (event.key === 'ArrowLeft') {
         changeSlide(-1);
+    }
+});
+
+const noteFrequencies = {
+    'G': [392, 1],
+    'A3': [440, 1.5],
+    'A2': [440, 0.5],
+    'B2': [493.88, 0.5],
+    'B3': [493.88, 1.5],
+    'E': [659.26, 1],
+    'D': [587.33, 1],
+    'Blank': [0, 0]
+};
+
+function playNote(frequency, duration) {
+    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    const oscillator = audioContext.createOscillator();
+    oscillator.type = 'sine';
+    oscillator.frequency.setValueAtTime(frequency, audioContext.currentTime);
+    oscillator.connect(audioContext.destination);
+    oscillator.start();
+    oscillator.stop(audioContext.currentTime + (duration * music_dur));
+}
+
+document.querySelectorAll('.note').forEach(noteDiv => {
+    noteDiv.addEventListener('click', () => {
+        const note = noteDiv.getAttribute('data-note');
+        const frequency = noteFrequencies[note];
+        playNote(frequency);
+    });
+});
+
+let audio;
+
+document.getElementById('audio-pic').addEventListener('click', function() {
+    console.log('<3');
+    if (!audio) {
+        audio = new Audio('lana.mp3');
+    }
+    audio.play();
+});
+
+window.addEventListener('beforeunload', function() {
+    if (audio) {
+        audio.pause();
+        audio.currentTime = 0;
     }
 });
